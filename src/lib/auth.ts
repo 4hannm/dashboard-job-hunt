64f56eb4995166a84f8eb69389920462c8f170bf
hp <144525103+4hannm@users.noreply.github.com>
2025-07-12 18:00:30 +0800

    still eror 2

---
+++ src/lib/auth.ts
@@ -0,0 +1,48 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../lib/prisma";
+import { comparePassword } from "@/lib/utils";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.company.findFirst({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+
+        const isMatch = await comparePassword(credentials.password, user.password);
+        if (!isMatch) return null;
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup",
+  },
+  callbacks: {
+    jwt({ token, account, user }) {
+      if (account && user) {
+        token.id = (user as any).id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user && token?.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+};
